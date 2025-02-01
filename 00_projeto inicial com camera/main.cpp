7c1d21db73655c54f9f076f0fe585056c4d42856
Antônio Andson da Silva <andsonsilva@alu.ufc.br>
2025-02-01 00:56:33 -0300

    criando função desenhar()
    agora sim começando o projeto do zero.

--- 00_projeto inicial com camera/main.cpp
+++ 00_projeto inicial com camera/main.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 #include <gui.h>
 
+void desenhar(){
+    GUI::displayInit();
+    GUI::displayEnd();
+}
 
 int main()
 {
     cout << "Hello World!" << endl;
 
-    GUI gui = GUI(800,600);
+    GUI gui = GUI(800,600,desenhar);
 }
 
 
